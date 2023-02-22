30229254f752391a3ae6ce1909fcfe4fe78d24f1
@ahmedyazid.mejri <ahmedyazid.mejri@esprit.tn>
2023-02-22 13:19:33 +0100

    fixing responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -31,7 +31,7 @@ export class AppComponent {
   openDialog() {
     this.dialog.open(DialogComponent, {
       width: '50%',
-      height: '50%',
+      height: '60%',
     });
   }
   ngOnInit(): void {
